6b4418888820494c5311617384e83f71ab82691b
locxter <54595101+locxter@users.noreply.github.com>
2022-10-28 15:08:50 +0200

    Embrace the dark mode.

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,7 +1,7 @@
 <script>
     'use strict';
 
-    import 'water.css/out/water.min.css';
+    import 'water.css/out/dark.min.css';
     import Router from 'svelte-spa-router';
     import routes from './lib/routes.js';
 </script>
